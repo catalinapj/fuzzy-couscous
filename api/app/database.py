a28c685ee8c3878cb59aa151a9bd0c1e13ed2f2d
catalinapj <catalinadamian71@gmail.com>
2026-03-03 12:48:03 +0000

    create users table

--- api/app/database.py
+++ api/app/database.py
@@ -1,9 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://admin:secret@chat-postgres:5432/chatapp"
+DB_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://admin:secret@localhost:5432/chatapp"
+)
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+engine = create_engine(DB_URL, pool_pre_ping=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
